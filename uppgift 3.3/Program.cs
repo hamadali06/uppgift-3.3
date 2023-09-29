600664ac43fdf3038593c73299dcddb77645f9c3
hamadali06 <ahal06003@utb.vaxjo.se>
2023-09-29 11:20:47 +0200

    Add project files.

---
+++ uppgift 3.3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hur många timmar vill du hyra en bil? ");
+            int antalTimmar = Convert.ToInt32(Console.ReadLine());
+
+            int maxKostnadPerDag = 950;
+            int timpris = 80;
+
+            int totalKostnad = antalTimmar <= 12 ? timpris * antalTimmar : maxKostnadPerDag;
+            Console.WriteLine($"Kostnaden för {antalTimmar} timmars bilhyra är {totalKostnad} kr.");
+        }
+    }
+}
